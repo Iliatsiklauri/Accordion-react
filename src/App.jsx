7f0f11f1e428e1052d1b6e5d464b1bdbcff5c50c
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-01-25 18:36:36 +0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ const App = () => {
   return (
     <div className="wrapper">
       <div className="boxWrapper">
-        <img src="public\Group 4 (4).svg" alt="" className="img1" />
-        <img src="public\Path Copy (1).svg" alt="" className="img2" />
-        <img src="public\mention_isometric 2 (1).svg" alt="" className="img3" />
-        <img src="public\Group 6 (3).svg" alt="" className="img4" />
-        <img src="public\Group 2 (4).svg" alt="" className="img5" />
+        <img src="\public\Group 4 (4).svg" alt="" className="img1" />
+        <img src="\public\Path Copy (1).svg" alt="" className="img2" />
+        <img src="\public\mention_isometric 2 (1).svg" alt="" className="img3" />
+        <img src="\public\Group 2 (4).svg" alt="" className="img5" />
 
         <h1>FAQ</h1>
         <Accordion />
       </div>
+      <img src="\public\Group 6 (3).svg" alt="" className="img4" />
     </div>
   );
 };
